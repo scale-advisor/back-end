8ce44b4710fbfed2e731c747683f0f46cefbdb5f
dj.jeong <sam1087@mju.ac.kr>
2025-04-26 20:32:08 +0900

    Initial Commit

---
+++ src/test/kotlin/org/scaleadvisor/backend/BackEndApplicationTests.kt
@@ -0,0 +1,13 @@
+package org.scaleadvisor.backend
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BackEndApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
