d4830eb66558f87dac14dfa57fefeab24078708d
wambatcode <feyfey927@gmail.com>
2025-05-05 17:48:24 +0900

    feat: User 도메인에 대한 Controller, Service, Repository 클래스 생성

---
+++ src/main/kotlin/org/scaleadvisor/backend/user/UserController.kt
@@ -0,0 +1,24 @@
+package org.scaleadvisor.backend.user
+
+import org.scaleadvisor.backend.user.dto.UserCreateRequest
+import org.scaleadvisor.backend.user.dto.UserResponse
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.*
+
+@RestController
+@RequestMapping("/users")
+class UserController(
+    private val userService: UserService
+) {
+    @GetMapping("/{id}")
+    fun getUser(@PathVariable id: Long): UserResponse {
+        return userService.getUser(id)
+    }
+
+    @PostMapping
+    fun createUser(@RequestBody request: UserCreateRequest): ResponseEntity<Long> {
+        val userId = userService.createUser(request.username, request.email, request.password)
+        return ResponseEntity.ok(userId)
+    }
+}
+
---
+++ src/main/kotlin/org/scaleadvisor/backend/user/UserRepository.kt
@@ -0,0 +1,37 @@
+package org.scaleadvisor.backend.user
+
+import org.jooq.DSLContext
+import org.jooq.generated.tables.Users.USERS
+import org.scaleadvisor.backend.user.dto.UserResponse
+import org.springframework.stereotype.Repository
+
+@Repository
+class UserRepository(
+    private val dsl: DSLContext
+) {
+
+    fun insertUser(username: String, email: String, password: String): Long {
+        val record = dsl.insertInto(USERS)
+            .set(USERS.USERNAME, username)
+            .set(USERS.EMAIL, email)
+            .set(USERS.PASSWORD, password)
+            .returningResult(USERS.ID)
+            .fetchOne()
+
+        return record?.value1() ?: throw RuntimeException("User 추가 실패")
+    }
+
+    fun findUserById(id: Long): UserResponse? {
+        val record = dsl.selectFrom(USERS)
+            .where(USERS.ID.eq(id))
+            .fetchOne()
+
+        return record?.let {
+            UserResponse(
+                id = it.id,
+                username = it.username,
+                email = it.email
+            )
+        }
+    }
+}
