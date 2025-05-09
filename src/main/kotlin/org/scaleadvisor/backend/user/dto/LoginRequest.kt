0ca0c1fdc36f05dbba5c39e3b5f8824eacb72e70
wambatcode <feyfey927@gmail.com>
2025-05-10 06:45:23 +0900

    feat: 회원가입, 로그인 Request, Response 클래스 작성

--- src/main/kotlin/org/scaleadvisor/backend/user/dto/LoginRequest.kt
+++ src/main/kotlin/org/scaleadvisor/backend/user/dto/LoginRequest.kt
@@ -1,7 +1,6 @@
 package org.scaleadvisor.backend.user.dto
 
-data class UserCreateRequest(
-    val username: String,
+data class LoginRequest(
     val email: String,
     val password: String
 )
--- src/main/kotlin/org/scaleadvisor/backend/user/dto/UserResponse.kt
+++ src/main/kotlin/org/scaleadvisor/backend/user/dto/UserResponse.kt
@@ -1,7 +1,22 @@
 package org.scaleadvisor.backend.user.dto
 
+import org.scaleadvisor.backend.user.domain.User
+import java.time.LocalDateTime
+
 data class UserResponse(
-    val id: Long,
-    val username: String,
-    val email: String
-)
+    val userId: Long,
+    val email: String,
+    val name: String,
+    val createdAt: LocalDateTime,
+    val updatedAt: LocalDateTime
+) {
+    companion object {
+        fun from(user: User) = UserResponse(
+            userId = user.userId!!,
+            email = user.email,
+            name = user.name,
+            createdAt = user.createdAt,
+            updatedAt = user.updatedAt
+        )
+    }
+}
