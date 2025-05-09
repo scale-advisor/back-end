fef5d68a10741658691fb178608c205fbb612d72
wambatcode <feyfey927@gmail.com>
2025-05-10 07:58:40 +0900

    Feat: AuthService에 PasswordEncoder 적용

--- src/main/kotlin/org/scaleadvisor/backend/user/service/AuthService.kt
+++ src/main/kotlin/org/scaleadvisor/backend/user/service/AuthService.kt
@@ -5,11 +5,15 @@ import org.scaleadvisor.backend.user.dto.LoginRequest
 import org.scaleadvisor.backend.user.dto.SignUpRequest
 import org.scaleadvisor.backend.user.repository.UserRepository
 import org.scaleadvisor.backend.user.dto.UserResponse
+import org.springframework.security.crypto.password.PasswordEncoder
 import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
 
 @Service
+@Transactional
 class AuthService(
-    private val userRepository: UserRepository
+    private val userRepository: UserRepository,
+    private val passwordEncoder: PasswordEncoder
 ) {
 
     fun signup(request: SignUpRequest): Long {
@@ -17,9 +21,11 @@ class AuthService(
             throw IllegalArgumentException("이미 사용 중인 이메일입니다.")
         }
 
+        val encodedPassword = passwordEncoder.encode(request.password)
+
         val newUser = User.of(
             email = request.email,
-            password = request.password,
+            password = encodedPassword,
             name = request.name,
             loginType = User.LoginType.BASIC
         )
@@ -30,6 +36,10 @@ class AuthService(
         val user = userRepository
             .findByEmail(request.email)
             ?: throw IllegalArgumentException("이메일 또는 비밀번호가 올바르지 않습니다.")
+
+        if (!passwordEncoder.matches(request.password, user.password)) {
+            throw IllegalArgumentException("이메일 또는 비밀번호가 올바르지 않습니다.")
+        }
 
         return UserResponse.from(user)
     }
