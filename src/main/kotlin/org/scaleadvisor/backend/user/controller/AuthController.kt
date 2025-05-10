ad63de53b2973475cf586b9c2561325430a63889
wambatcode <feyfey927@gmail.com>
2025-05-10 14:33:10 +0900

    feat: 토큰 재발급 Api 추가

--- src/main/kotlin/org/scaleadvisor/backend/user/controller/AuthController.kt
+++ src/main/kotlin/org/scaleadvisor/backend/user/controller/AuthController.kt
@@ -4,6 +4,7 @@ import org.scaleadvisor.backend.user.dto.LoginRequest
 import org.scaleadvisor.backend.user.service.AuthService
 import org.scaleadvisor.backend.user.dto.SignUpRequest
 import org.scaleadvisor.backend.user.dto.LoginResponse
+import org.scaleadvisor.backend.user.dto.RefreshRequest
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
 
@@ -22,6 +23,12 @@ class AuthController(
     fun login(@RequestBody request: LoginRequest): ResponseEntity<LoginResponse> {
         val user = authService.login(request)
         return ResponseEntity.ok().body(user)
+    }
+
+    @PostMapping("/refresh")
+    fun refresh(@RequestBody request: RefreshRequest): ResponseEntity<LoginResponse> {
+        val refresh = authService.refreshToken(request.refreshToken)
+        return ResponseEntity.ok().body(refresh)
     }
 }
 
