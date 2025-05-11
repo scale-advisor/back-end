6bff5ce85e541f6e86ca39b44100966964c94247
wambatcode <feyfey927@gmail.com>
2025-05-12 04:00:00 +0900

    Feat: RestTemplate Config 추가

---
+++ src/main/kotlin/org/scaleadvisor/backend/global/config/RestTemplateConfig.kt
@@ -0,0 +1,13 @@
+package org.scaleadvisor.backend.global.config
+
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.client.RestTemplate
+
+
+@Configuration
+class RestTemplateConfig {
+
+    @Bean
+    fun restTemplate(): RestTemplate = RestTemplate()
+}
