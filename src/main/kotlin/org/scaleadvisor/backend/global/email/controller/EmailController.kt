e7bbe0dfa04ac741e1757b9befb1ff5d2b7a09ca
wambatcode <feyfey927@gmail.com>
2025-05-14 07:23:48 +0900

    feat: resetPasswordEmail Api 및 dto 생성

--- src/main/kotlin/org/scaleadvisor/backend/global/email/controller/EmailController.kt
+++ src/main/kotlin/org/scaleadvisor/backend/global/email/controller/EmailController.kt
@@ -1,5 +1,6 @@
 package org.scaleadvisor.backend.global.email.controller
 
+import org.scaleadvisor.backend.global.email.dto.PwdResetRequest
 import org.scaleadvisor.backend.global.email.service.EmailService
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
@@ -12,4 +13,8 @@ class EmailController(
     @GetMapping("/email-verification")
     fun verifyEmail(@RequestParam email: String, @RequestParam token: String
     ): ResponseEntity<String> = emailService.confirmSignup(email, token)
+
+    @PostMapping("/password-reset/request")
+    fun resetPasswordEmail(@RequestBody request: PwdResetRequest
+    ): ResponseEntity<String> = emailService.sendResetPasswordEmail(request)
 }
---
+++ src/main/kotlin/org/scaleadvisor/backend/global/email/dto/PwdResetRequest.kt
@@ -0,0 +1,6 @@
+package org.scaleadvisor.backend.global.email.dto
+
+data class PwdResetRequest (
+    val email: String,
+    val appUrl: String
+)
