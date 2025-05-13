d5364c3dfb8c21e183189fe425ba367aaed681a6
wambatcode <feyfey927@gmail.com>
2025-05-14 08:54:59 +0900

    feat: 비밀번호 초기화 관련 GET, POST API 추가

--- src/main/kotlin/org/scaleadvisor/backend/global/email/controller/EmailController.kt
+++ src/main/kotlin/org/scaleadvisor/backend/global/email/controller/EmailController.kt
@@ -1,9 +1,12 @@
 package org.scaleadvisor.backend.global.email.controller
 
+import jakarta.servlet.http.HttpServletResponse
+import org.scaleadvisor.backend.global.email.dto.PwdResetConfirmRequest
 import org.scaleadvisor.backend.global.email.dto.PwdResetRequest
 import org.scaleadvisor.backend.global.email.service.EmailService
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
+import org.springframework.web.servlet.view.RedirectView
 
 @RestController
 @RequestMapping("/auth")
@@ -17,4 +20,18 @@ class EmailController(
     @PostMapping("/password-reset/request")
     fun resetPasswordEmail(@RequestBody request: PwdResetRequest
     ): ResponseEntity<String> = emailService.sendResetPasswordEmail(request)
+
+    @GetMapping("/password-reset")
+    fun verifyResetToken(@RequestParam token: String, response: HttpServletResponse
+    ): RedirectView {
+        val redirectUrl = emailService.confirmResetToken(token)!!
+        return RedirectView(redirectUrl)
+    }
+
+    @PostMapping("/password-reset")
+    fun resetPassword(@RequestBody request: PwdResetConfirmRequest
+    ): ResponseEntity<String> {
+        emailService.resetPassword(request)
+        return ResponseEntity.ok().body("비밀번호가 성공적으로 변경되었습니다.")
+    }
 }
