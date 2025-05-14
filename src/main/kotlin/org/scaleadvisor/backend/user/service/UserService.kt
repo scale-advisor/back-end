f9705091f350a067e94335dde637284ab7329ea4
wambatcode <feyfey927@gmail.com>
2025-05-14 14:38:44 +0900

    refactor: users Api 메소드 및 UserService 리팩토링

--- src/main/kotlin/org/scaleadvisor/backend/user/service/UserService.kt
+++ src/main/kotlin/org/scaleadvisor/backend/user/service/UserService.kt
@@ -1,7 +1,9 @@
 package org.scaleadvisor.backend.user.service
 
 import org.scaleadvisor.backend.global.exception.constant.UserMessageConstant
+import org.scaleadvisor.backend.global.exception.model.ForbiddenException
 import org.scaleadvisor.backend.global.exception.model.NotFoundException
+import org.scaleadvisor.backend.global.security.CurrentUserIdExtractor
 import org.scaleadvisor.backend.user.repository.UserRepository
 import org.springframework.stereotype.Service
 
@@ -9,15 +11,18 @@ import org.springframework.stereotype.Service
 class UserService(
     private val userRepository: UserRepository
 ) {
-    fun updateName(userId: Long, newName: String) {
-        if (userRepository.updateNameById(userId, newName) != 1){
-            throw NotFoundException(String.format(UserMessageConstant.NOT_FOUND_USER_ID_MESSAGE, userId))
+    private val currentUserId: Long = CurrentUserIdExtractor.getCurrentUserIdFromSecurity()
+        ?: throw ForbiddenException("현재 인증이 되지 않은 접근 입니다.")
+
+    fun updateName(newName: String) {
+        if (userRepository.updateNameById(currentUserId, newName) != 1){
+            throw NotFoundException(String.format(UserMessageConstant.NOT_FOUND_USER_ID_MESSAGE, currentUserId))
         }
     }
 
-    fun deleteUser(userId: Long) {
-        if (userRepository.deleteUser(userId) != 1) {
-            throw NotFoundException(String.format(UserMessageConstant.NOT_FOUND_USER_ID_MESSAGE, userId))
+    fun deleteUser() {
+        if (userRepository.deleteUser(currentUserId) != 1) {
+            throw NotFoundException(String.format(UserMessageConstant.NOT_FOUND_USER_ID_MESSAGE, currentUserId))
         }
     }
 }
