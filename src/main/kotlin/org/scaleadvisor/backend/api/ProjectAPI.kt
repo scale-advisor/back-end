455895d3c2a2c66f5a1dbc5270071c652a723c61
dj.jeong <sam1087@mju.ac.kr>
2025-05-19 02:08:44 +0900

    feat: ProjectAPI findAll 메소드 추가

--- src/main/kotlin/org/scaleadvisor/backend/api/ProjectAPI.kt
+++ src/main/kotlin/org/scaleadvisor/backend/api/ProjectAPI.kt
@@ -5,7 +5,9 @@ import io.swagger.v3.oas.annotations.tags.Tag
 import org.scaleadvisor.backend.api.response.SuccessResponse
 import org.scaleadvisor.backend.project.api.request.CreateProjectRequest
 import org.scaleadvisor.backend.project.api.response.CreateProjectResponse
+import org.scaleadvisor.backend.project.api.response.FindAllProjectResponse
 import org.springframework.http.HttpStatus
+import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.ResponseStatus
@@ -21,5 +23,13 @@ interface ProjectAPI {
     @PostMapping("")
     @ResponseStatus(HttpStatus.CREATED)
     fun create(request: CreateProjectRequest): SuccessResponse<CreateProjectResponse>
+
+    @Operation(
+        summary = "Project 목록 조회",
+        description = "로그인한 회원이 생성한/참여하고 있는 프로젝트 목록 조회"
+    )
+    @GetMapping("")
+    @ResponseStatus(HttpStatus.OK)
+    fun findAll(): SuccessResponse<FindAllProjectResponse>
 
 }
--- src/main/kotlin/org/scaleadvisor/backend/project/api/ProjectController.kt
+++ src/main/kotlin/org/scaleadvisor/backend/project/api/ProjectController.kt
@@ -4,6 +4,7 @@ import org.scaleadvisor.backend.api.ProjectAPI
 import org.scaleadvisor.backend.api.response.SuccessResponse
 import org.scaleadvisor.backend.project.api.request.CreateProjectRequest
 import org.scaleadvisor.backend.project.api.response.CreateProjectResponse
+import org.scaleadvisor.backend.project.api.response.FindAllProjectResponse
 import org.scaleadvisor.backend.project.application.port.usecase.CreateProjectUseCase
 import org.scaleadvisor.backend.project.domain.Project
 import org.springframework.web.bind.annotation.RestController
@@ -23,5 +24,12 @@ class ProjectController(
         return SuccessResponse.from(
             CreateProjectResponse.from(project)
         )
+    }
+
+    override fun findAll(): SuccessResponse<FindAllProjectResponse> {
+        TODO("Not yet implemented")
+//        val currentUserId = CurrentUserIdExtractor.getCurrentUserIdFromSecurity()
+//            ?: throw ForbiddenException("현재 인증이 되지 않은 접근 입니다.")
+
     }
 }
