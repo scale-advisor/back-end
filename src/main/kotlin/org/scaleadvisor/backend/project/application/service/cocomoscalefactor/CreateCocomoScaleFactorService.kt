5e48140314c9347bccd5c4437a206ea193bcd284
wambatcode <feyfey927@gmail.com>
2025-05-21 03:02:34 +0900

    feat: CocomoScaleFactor Create UseCase 및 Service 생성

---
+++ src/main/kotlin/org/scaleadvisor/backend/project/application/service/cocomoscalefactor/CreateCocomoScaleFactorService.kt
@@ -0,0 +1,35 @@
+package org.scaleadvisor.backend.project.application.service.cocomoscalefactor
+
+import org.scaleadvisor.backend.project.application.port.repository.cocomoscalefactor.CreateCocomoScaleFactorPort
+import org.scaleadvisor.backend.project.application.port.usecase.cocomoscalefactor.CreateCocomoScaleFactorUseCase
+import org.scaleadvisor.backend.project.domain.CocomoScaleFactor
+import org.scaleadvisor.backend.project.domain.id.CocomoScaleFactorId
+import org.springframework.stereotype.Service
+import java.time.LocalDateTime
+
+@Service
+private class CreateCocomoScaleFactorService(
+    private val createCocomoScaleFactorPort: CreateCocomoScaleFactorPort
+): CreateCocomoScaleFactorUseCase {
+    override fun create(command: CreateCocomoScaleFactorUseCase.CreateCocomoScaleFactorCommand
+    ): CocomoScaleFactor {
+        val newId = CocomoScaleFactorId.newId()
+        val now = LocalDateTime.now()
+
+        val cocomoScaleFactor = CocomoScaleFactor(
+            cocomoScaleFactorId = newId,
+            projectId = command.projectId,
+            prec = command.prec,
+            flex = command.flex,
+            resl = command.resl,
+            team = command.team,
+            pmat = command.pmat,
+            createdAt = now,
+            updatedAt = now
+        )
+
+        createCocomoScaleFactorPort.create(cocomoScaleFactor)
+
+        return cocomoScaleFactor
+    }
+}
