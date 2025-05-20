a4da200f639873ffa8ae8dcffdf839f100afc53f
wambatcode <feyfey927@gmail.com>
2025-05-21 04:24:46 +0900

    feat: CocomoScaleFactor update Api 생성

--- src/main/kotlin/org/scaleadvisor/backend/project/api/CocomoController.kt
+++ src/main/kotlin/org/scaleadvisor/backend/project/api/CocomoController.kt
@@ -4,17 +4,23 @@ import org.scaleadvisor.backend.api.CocomoApi
 import org.scaleadvisor.backend.api.response.SuccessResponse
 import org.scaleadvisor.backend.global.exception.model.NotFoundException
 import org.scaleadvisor.backend.project.api.request.CreateCocomoScaleFactorRequest
+import org.scaleadvisor.backend.project.api.request.UpdateCocomoScaleFactorRequest
 import org.scaleadvisor.backend.project.api.response.CreateCocomoScaleFactorResponse
 import org.scaleadvisor.backend.project.api.response.FindCocomoScaleFactorResponse
+import org.scaleadvisor.backend.project.api.response.UpdateCocomoScaleFactorResponse
 import org.scaleadvisor.backend.project.application.port.usecase.cocomoscalefactor.CreateCocomoScaleFactorUseCase
 import org.scaleadvisor.backend.project.application.port.usecase.cocomoscalefactor.FindCocomoScaleFactorUseCase
+import org.scaleadvisor.backend.project.application.port.usecase.cocomoscalefactor.UpdateCocomoScaleFactorUseCase
+import org.scaleadvisor.backend.project.application.port.usecase.project.GetProjectUseCase
 import org.scaleadvisor.backend.project.domain.id.ProjectId
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
 private class CocomoController(
     private val createCocomoScaleFactorUseCase: CreateCocomoScaleFactorUseCase,
-    private val findCocomoScaleFactorUseCase: FindCocomoScaleFactorUseCase
+    private val findCocomoScaleFactorUseCase: FindCocomoScaleFactorUseCase,
+    private val updateCocomoScaleFactorUseCase: UpdateCocomoScaleFactorUseCase,
+    private val getProjectUseCase: GetProjectUseCase
 ): CocomoApi {
 
     override fun createCocomoScaleFactor(
@@ -45,6 +51,29 @@ private class CocomoController(
 
         return SuccessResponse.from(
             FindCocomoScaleFactorResponse.from(cocomoScaleFactor)
+        )
+    }
+
+    override fun updateCocomoScaleFactor(
+        projectId: Long,
+        request: UpdateCocomoScaleFactorRequest
+    ): SuccessResponse<UpdateCocomoScaleFactorResponse> {
+        getProjectUseCase.find(ProjectId.of(projectId))
+            ?: throw NotFoundException("프로젝트를 찾을 수 없습니다. (id=$projectId)")
+
+        val updated = updateCocomoScaleFactorUseCase.update(
+            UpdateCocomoScaleFactorUseCase.UpdateCocomoScaleFactorCommand(
+                projectId = ProjectId.of(projectId),
+                prec = request.prec,
+                flex = request.flex,
+                resl = request.resl,
+                team = request.team,
+                pmat = request.pmat
+            )
+        )
+
+        return SuccessResponse.from(
+            UpdateCocomoScaleFactorResponse.from(updated)
         )
     }
 }
--- src/main/kotlin/org/scaleadvisor/backend/project/domain/CocomoScaleFactor.kt
+++ src/main/kotlin/org/scaleadvisor/backend/project/domain/CocomoScaleFactor.kt
@@ -9,11 +9,26 @@ data class CocomoScaleFactor(
     val cocomoScaleFactorId: CocomoScaleFactorId,
     val projectId: ProjectId,
 
-    val prec: CocomoScaleFactorLevel,
-    val flex: CocomoScaleFactorLevel,
-    val resl: CocomoScaleFactorLevel,
-    val team: CocomoScaleFactorLevel,
-    val pmat: CocomoScaleFactorLevel,
+    var prec: CocomoScaleFactorLevel,
+    var flex: CocomoScaleFactorLevel,
+    var resl: CocomoScaleFactorLevel,
+    var team: CocomoScaleFactorLevel,
+    var pmat: CocomoScaleFactorLevel,
     val createdAt: LocalDateTime,
     var updatedAt: LocalDateTime?
-)
+){
+    fun update(
+        prec: CocomoScaleFactorLevel,
+        flex: CocomoScaleFactorLevel,
+        resl: CocomoScaleFactorLevel,
+        team: CocomoScaleFactorLevel,
+        pmat: CocomoScaleFactorLevel
+    ) {
+        this.prec = prec
+        this.flex = flex
+        this.resl = resl
+        this.team = team
+        this.pmat = pmat
+        this.updatedAt = LocalDateTime.now()
+    }
+}
