87babc6c83630e499497fe89cb5683e547de7fda
wambatcode <feyfey927@gmail.com>
2025-05-21 16:19:08 +0900

    feat: CocomoMultiplier Find(Get) UseCase 및 Service 생성

---
+++ src/main/kotlin/org/scaleadvisor/backend/project/application/service/cocomomultiplier/FindCocomoMultiplierService.kt
@@ -0,0 +1,17 @@
+package org.scaleadvisor.backend.project.application.service.cocomomultiplier
+
+import org.scaleadvisor.backend.project.application.port.repository.cocomomultiplier.FindCocomoMultiplierPort
+import org.scaleadvisor.backend.project.application.port.usecase.cocomomultiplier.FindCocomoMultiplierUseCase
+import org.scaleadvisor.backend.project.domain.CocomoMultiplier
+import org.scaleadvisor.backend.project.domain.id.ProjectId
+import org.springframework.stereotype.Service
+
+@Service
+private class FindCocomoMultiplierService(
+    private val findCocomoMultiplierPort: FindCocomoMultiplierPort
+): FindCocomoMultiplierUseCase {
+
+    override fun find(projectId: ProjectId): CocomoMultiplier? {
+        return findCocomoMultiplierPort.findByProjectId(projectId)
+    }
+}
