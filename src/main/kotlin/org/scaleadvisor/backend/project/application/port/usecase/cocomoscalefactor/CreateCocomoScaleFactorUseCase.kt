c2133bb1ce26e6bf8252c163434636cd0a976ed0
wambatcode <feyfey927@gmail.com>
2025-05-21 15:10:53 +0900

    rename: cocomo 계수 enum 파일명 변경

--- src/main/kotlin/org/scaleadvisor/backend/project/application/port/usecase/cocomoscalefactor/CreateCocomoScaleFactorUseCase.kt
+++ src/main/kotlin/org/scaleadvisor/backend/project/application/port/usecase/cocomoscalefactor/CreateCocomoScaleFactorUseCase.kt
@@ -1,17 +1,17 @@
 package org.scaleadvisor.backend.project.application.port.usecase.cocomoscalefactor
 
 import org.scaleadvisor.backend.project.domain.CocomoScaleFactor
-import org.scaleadvisor.backend.project.domain.enum.CocomoScaleFactorLevel
+import org.scaleadvisor.backend.project.domain.enum.CocomoLevel
 import org.scaleadvisor.backend.project.domain.id.ProjectId
 
 fun interface CreateCocomoScaleFactorUseCase {
     class CreateCocomoScaleFactorCommand(
         val projectId: ProjectId,
-        val prec: CocomoScaleFactorLevel,
-        val flex: CocomoScaleFactorLevel,
-        val resl: CocomoScaleFactorLevel,
-        val team: CocomoScaleFactorLevel,
-        val pmat: CocomoScaleFactorLevel
+        val prec: CocomoLevel,
+        val flex: CocomoLevel,
+        val resl: CocomoLevel,
+        val team: CocomoLevel,
+        val pmat: CocomoLevel
     )
 
     fun create(command: CreateCocomoScaleFactorCommand): CocomoScaleFactor
--- src/main/kotlin/org/scaleadvisor/backend/project/application/port/usecase/cocomoscalefactor/UpdateCocomoScaleFactorUseCase.kt
+++ src/main/kotlin/org/scaleadvisor/backend/project/application/port/usecase/cocomoscalefactor/UpdateCocomoScaleFactorUseCase.kt
@@ -1,17 +1,17 @@
 package org.scaleadvisor.backend.project.application.port.usecase.cocomoscalefactor
 
 import org.scaleadvisor.backend.project.domain.CocomoScaleFactor
-import org.scaleadvisor.backend.project.domain.enum.CocomoScaleFactorLevel
+import org.scaleadvisor.backend.project.domain.enum.CocomoLevel
 import org.scaleadvisor.backend.project.domain.id.ProjectId
 
 fun interface UpdateCocomoScaleFactorUseCase {
     class UpdateCocomoScaleFactorCommand(
         val projectId: ProjectId,
-        val prec: CocomoScaleFactorLevel,
-        val flex: CocomoScaleFactorLevel,
-        val resl: CocomoScaleFactorLevel,
-        val team: CocomoScaleFactorLevel,
-        val pmat: CocomoScaleFactorLevel
+        val prec: CocomoLevel,
+        val flex: CocomoLevel,
+        val resl: CocomoLevel,
+        val team: CocomoLevel,
+        val pmat: CocomoLevel
     )
 
     fun update(command: UpdateCocomoScaleFactorCommand): CocomoScaleFactor
