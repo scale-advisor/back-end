542e46af626c7e845f7b9f0a2d15c75a16275e01
wambatcode <feyfey927@gmail.com>
2025-05-21 17:00:37 +0900

    feat: CocomoMultiplier Update UseCase 및 Service 생성

---
+++ src/main/kotlin/org/scaleadvisor/backend/project/application/service/cocomomultiplier/UpdateCocomoMultiplierService.kt
@@ -0,0 +1,33 @@
+package org.scaleadvisor.backend.project.application.service.cocomomultiplier
+
+import org.scaleadvisor.backend.global.exception.model.NotFoundException
+import org.scaleadvisor.backend.project.application.port.repository.cocomomultiplier.UpdateCocomoMultiplierPort
+import org.scaleadvisor.backend.project.application.port.usecase.cocomomultiplier.FindCocomoMultiplierUseCase
+import org.scaleadvisor.backend.project.application.port.usecase.cocomomultiplier.UpdateCocomoMultiplierUseCase
+import org.scaleadvisor.backend.project.domain.CocomoMultiplier
+import org.springframework.stereotype.Service
+
+@Service
+private class UpdateCocomoMultiplierService(
+    private val updateCocomoMultiplierPort: UpdateCocomoMultiplierPort,
+    private val findCocomoMultiplierUseCase: FindCocomoMultiplierUseCase
+): UpdateCocomoMultiplierUseCase {
+    override fun update(command: UpdateCocomoMultiplierUseCase.UpdateCocomoMultiplierCommand
+    ): CocomoMultiplier {
+        val cocomoMultiplier = findCocomoMultiplierUseCase.find(command.projectId)
+            ?: throw NotFoundException("프로젝트를 찾을 수 없습니다. (id=$command.projectId)")
+
+        cocomoMultiplier.update(
+            rcpx = command.rcpx,
+            ruse = command.ruse,
+            pdif = command.pdif,
+            pers = command.pers,
+            sced = command.sced,
+            fcil = command.fcil
+        )
+
+        updateCocomoMultiplierPort.update(cocomoMultiplier)
+
+        return cocomoMultiplier
+    }
+}
