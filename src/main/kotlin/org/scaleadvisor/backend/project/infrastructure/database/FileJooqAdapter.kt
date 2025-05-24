d8b448cf8939aa9c1f87fd9338bdcfefb95ea013
dj.jeong <sam1087@mju.ac.kr>
2025-05-25 00:10:08 +0900

    feat: File 메타 정보 조회 API 추가

--- src/main/kotlin/org/scaleadvisor/backend/project/infrastructure/database/FileJooqAdapter.kt
+++ src/main/kotlin/org/scaleadvisor/backend/project/infrastructure/database/FileJooqAdapter.kt
@@ -2,14 +2,32 @@ package org.scaleadvisor.backend.project.infrastructure.database
 
 import org.jooq.DSLContext
 import org.jooq.generated.Tables.FILE
+import org.jooq.generated.tables.records.FileRecord
 import org.scaleadvisor.backend.project.application.port.repository.file.CreateFilePort
+import org.scaleadvisor.backend.project.application.port.repository.file.GetFilePort
 import org.scaleadvisor.backend.project.domain.File
+import org.scaleadvisor.backend.project.domain.enum.FileType
+import org.scaleadvisor.backend.project.domain.id.FileId
+import org.scaleadvisor.backend.project.domain.id.ProjectId
 import org.springframework.stereotype.Repository
 
 @Repository
 private class FileJooqAdapter(
     private val dsl: DSLContext
-) : CreateFilePort {
+) : CreateFilePort, GetFilePort {
+
+    private fun FileRecord.toDomain() = File(
+        id = FileId.of(this.fileId),
+        projectId = ProjectId.of(this.projectId),
+        versionNumber = this.versionNumber,
+        name = this.name,
+        type = FileType.valueOf(this.type),
+        uploaderId = this.uploaderId,
+        path = this.path,
+        extension = this.extension,
+        createdAt = this.createdAt,
+        updatedAt = this.updatedAt
+    )
 
     override fun create(file: File) {
         dsl.insertInto(FILE)
@@ -24,6 +42,16 @@ private class FileJooqAdapter(
             .set(FILE.CREATED_AT, file.createdAt)
             .set(FILE.UPDATED_AT, file.updatedAt)
             .execute()
+    }
+
+    override fun find(
+        projectId: ProjectId,
+        versionNumber: String
+    ): File? {
+        return dsl.selectFrom(FILE)
+            .where(FILE.PROJECT_ID.eq(projectId.toLong()))
+            .and(FILE.VERSION_NUMBER.eq(versionNumber))
+            .fetchOne { record -> record.into(FILE).toDomain() }
     }
 
 }
