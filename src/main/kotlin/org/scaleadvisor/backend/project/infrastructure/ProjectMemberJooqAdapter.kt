0f026ac3690e96925276daefcfc304eb03214ba5
wambatcode <feyfey927@gmail.com>
2025-05-24 17:26:45 +0900

    fix: ProjectMember GetAll Adapter 메소드 수정

--- src/main/kotlin/org/scaleadvisor/backend/project/infrastructure/ProjectMemberJooqAdapter.kt
+++ src/main/kotlin/org/scaleadvisor/backend/project/infrastructure/ProjectMemberJooqAdapter.kt
@@ -3,15 +3,18 @@ package org.scaleadvisor.backend.project.infrastructure
 import org.jooq.DSLContext
 import org.jooq.generated.Tables.PROJECT_MEMBER
 import org.jooq.generated.Tables.USER
+import org.jooq.impl.DSL
 import org.scaleadvisor.backend.project.application.port.repository.member.CreateProjectMemberPort
 import org.scaleadvisor.backend.project.application.port.repository.member.DeleteProjectMemberPort
 import org.scaleadvisor.backend.project.application.port.repository.member.GetAllProjectMemberPort
 import org.scaleadvisor.backend.project.application.port.repository.member.GetProjectMemberPort
-import org.scaleadvisor.backend.project.domain.ProjectMember
+import org.scaleadvisor.backend.project.domain.*
 import org.scaleadvisor.backend.project.domain.enum.MemberRole
 import org.scaleadvisor.backend.project.domain.enum.MemberState
 import org.scaleadvisor.backend.project.domain.id.ProjectId
+import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Repository
+
 
 @Repository
 private class ProjectMemberJooqAdapter(
@@ -21,10 +24,14 @@ private class ProjectMemberJooqAdapter(
     DeleteProjectMemberPort,
     GetAllProjectMemberPort{
 
+    private val log = LoggerFactory.getLogger(javaClass)
+
     override fun create(userId: Long, projectId: ProjectId) {
         dsl.insertInto(PROJECT_MEMBER)
             .set(PROJECT_MEMBER.USER_ID, userId)
             .set(PROJECT_MEMBER.PROJECT_ID, projectId.toLong())
+            .set(PROJECT_MEMBER.STATE,      MemberState.ACCEPTED.name)
+            .set(PROJECT_MEMBER.ROLE,       MemberRole.OWNER.name)
             .set(PROJECT_MEMBER.CREATED_AT, java.time.LocalDateTime.now())
             .set(PROJECT_MEMBER.UPDATED_AT, java.time.LocalDateTime.now())
             .execute()
@@ -58,7 +65,8 @@ private class ProjectMemberJooqAdapter(
     ): List<ProjectMember> {
         return dsl
             .select(
-                PROJECT_MEMBER.USER_ID,
+                USER.NAME,
+                USER.EMAIL,
                 PROJECT_MEMBER.PROJECT_ID,
                 PROJECT_MEMBER.STATE,
                 PROJECT_MEMBER.ROLE,
@@ -66,18 +74,21 @@ private class ProjectMemberJooqAdapter(
                 PROJECT_MEMBER.UPDATED_AT
             )
             .from(PROJECT_MEMBER)
-            .join(USER)
-            .on(PROJECT_MEMBER.USER_ID.eq(USER.USER_ID))
+            .join(USER).on(PROJECT_MEMBER.USER_ID.eq(USER.USER_ID))
             .where(PROJECT_MEMBER.PROJECT_ID.eq(projectId.toLong()))
-            .orderBy(USER.NAME.asc())
-            .limit(limit)
+            .orderBy(
+                PROJECT_MEMBER.ROLE.desc(),
+                USER.NAME.asc()
+            )
             .offset(offset)
+            .limit(limit)
             .fetch { record -> mapToProjectMember(record) }
     }
 
     private fun mapToProjectMember(record: org.jooq.Record): ProjectMember =
         ProjectMember(
-            userId    = record.get(PROJECT_MEMBER.USER_ID),
+            name      = record.get(USER.NAME),
+            email     = record.get(USER.EMAIL),
             projectId = ProjectId.of(record.get(PROJECT_MEMBER.PROJECT_ID)),
             state     = MemberState.valueOf(record.get(PROJECT_MEMBER.STATE)),
             role      = MemberRole.valueOf(record.get(PROJECT_MEMBER.ROLE)),
