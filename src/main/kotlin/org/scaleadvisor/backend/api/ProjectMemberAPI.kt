632b66b6e00d380343d4b3b052ebb40beaf1ca23
wambatcode <feyfey927@gmail.com>
2025-05-24 23:57:52 +0900

    HOTFIX: ProjectMemberAPI 버그 수정

--- src/main/kotlin/org/scaleadvisor/backend/api/ProjectMemberAPI.kt
+++ src/main/kotlin/org/scaleadvisor/backend/api/ProjectMemberAPI.kt
@@ -4,7 +4,7 @@ import io.swagger.v3.oas.annotations.tags.Tag
 import org.scaleadvisor.backend.api.response.SuccessResponse
 import org.scaleadvisor.backend.project.api.request.UpdateMemberRoleRequest
 import org.scaleadvisor.backend.project.api.response.GetAllProjectMemberResponse
-import org.scaleadvisor.backend.project.api.response.UpdateProjectMemberResponse
+import org.scaleadvisor.backend.project.api.response.UpdateMemberRoleResponse
 import org.springframework.http.HttpStatus
 import org.springframework.web.bind.annotation.*
 
@@ -23,5 +23,5 @@ interface ProjectMemberAPI {
     @ResponseStatus(HttpStatus.OK)
     fun updateMemberRole(@PathVariable projectId: Long,
                          @RequestBody request: UpdateMemberRoleRequest
-    ): SuccessResponse<UpdateProjectMemberResponse>
+    ): SuccessResponse<UpdateMemberRoleResponse>
 }
