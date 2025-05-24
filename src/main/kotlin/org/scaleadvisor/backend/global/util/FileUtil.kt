61274531123a72cb5e6f9c923321ee5799a92ba0
dj.jeong <sam1087@mju.ac.kr>
2025-05-24 22:16:53 +0900

    refactor: 코드 개선

--- src/main/kotlin/org/scaleadvisor/backend/global/util/FileUtil.kt
+++ src/main/kotlin/org/scaleadvisor/backend/global/util/FileUtil.kt
@@ -4,14 +4,13 @@ import org.springframework.web.multipart.MultipartFile
 
 object FileUtil {
     fun getExtension(file: MultipartFile): String {
-        val fileName = file.originalFilename
-        if (fileName == null) {
-            throw IllegalArgumentException("File name is null")
-        }
+        val fileName = requireNotNull(file.originalFilename) { "File name is null" }
+
         val dotIndex = fileName.lastIndexOf('.')
-        if (dotIndex == -1 || dotIndex == fileName.length - 1) {
-            throw IllegalArgumentException("File name has no extension")
+        require(!(dotIndex == -1 || dotIndex == fileName.length - 1)) {
+            "File name has no extension"
         }
+
         return fileName.substring(dotIndex + 1)
     }
 }
