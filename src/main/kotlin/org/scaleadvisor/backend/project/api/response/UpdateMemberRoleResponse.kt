96102952653fc7d6d65b3cff034fe0a59a645257
wambatcode <feyfey927@gmail.com>
2025-05-24 20:41:17 +0900

    rename: UpdateMemberRoleResponse 파일명 변경

--- src/main/kotlin/org/scaleadvisor/backend/project/api/response/UpdateMemberRoleResponse.kt
+++ src/main/kotlin/org/scaleadvisor/backend/project/api/response/UpdateMemberRoleResponse.kt
@@ -5,7 +5,7 @@ import org.scaleadvisor.backend.project.domain.enum.MemberRole
 import org.scaleadvisor.backend.project.domain.enum.MemberState
 import java.time.LocalDateTime
 
-data class UpdateProjectMemberResponse(
+data class UpdateMemberRoleResponse(
     val name: String,
     val email: String,
     val projectId: Long,
@@ -16,8 +16,8 @@ data class UpdateProjectMemberResponse(
 ) {
     companion object {
         @JvmStatic
-        fun from(member: ProjectMember): UpdateProjectMemberResponse =
-            UpdateProjectMemberResponse(
+        fun from(member: ProjectMember): UpdateMemberRoleResponse =
+            UpdateMemberRoleResponse(
                 name = member.name,
                 email = member.email,
                 projectId = member.projectId.toLong(),
