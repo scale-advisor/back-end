d1c429126f557b92392313103f50c44db387e9b2
wambatcode <feyfey927@gmail.com>
2025-05-24 19:12:19 +0900

    rename: GetProjectMember -> CheckProjectMember 파일명 변경

--- src/main/kotlin/org/scaleadvisor/backend/project/application/port/repository/member/CheckProjectMemberPort.kt
+++ src/main/kotlin/org/scaleadvisor/backend/project/application/port/repository/member/CheckProjectMemberPort.kt
@@ -2,7 +2,7 @@ package org.scaleadvisor.backend.project.application.port.repository.member
 
 import org.scaleadvisor.backend.project.domain.id.ProjectId
 
-fun interface GetProjectMemberPort {
+fun interface CheckProjectMemberPort {
 
     fun exist(userId: Long, projectId: ProjectId): Boolean
 
--- src/main/kotlin/org/scaleadvisor/backend/project/application/service/member/CheckIsProjectMemberService.kt
+++ src/main/kotlin/org/scaleadvisor/backend/project/application/service/member/CheckIsProjectMemberService.kt
@@ -1,18 +1,18 @@
 package org.scaleadvisor.backend.project.application.service.member
 
-import org.scaleadvisor.backend.project.application.port.repository.member.GetProjectMemberPort
+import org.scaleadvisor.backend.project.application.port.repository.member.CheckProjectMemberPort
 import org.scaleadvisor.backend.project.application.port.usecase.member.CheckIsProjectMemberUseCase
 import org.scaleadvisor.backend.project.domain.id.ProjectId
 import org.springframework.stereotype.Service
 
 @Service
 private class CheckIsProjectMemberService(
-    private val getUsersProjectPort: GetProjectMemberPort
+    private val checkUsersProjectPort: CheckProjectMemberPort
 ) : CheckIsProjectMemberUseCase {
     override fun isProjectMember(
         userId: Long,
         projectId: ProjectId
     ): Boolean {
-        return getUsersProjectPort.exist(userId, projectId)
+        return checkUsersProjectPort.exist(userId, projectId)
     }
 }
