f729df53f2a54471e879693d926a2408216561cb
wambatcode <feyfey927@gmail.com>
2025-05-24 20:10:27 +0900

    feat: ProjectMember Role Update Api 생성

--- src/main/kotlin/org/scaleadvisor/backend/api/ProjectMemberAPI.kt
+++ src/main/kotlin/org/scaleadvisor/backend/api/ProjectMemberAPI.kt
@@ -2,7 +2,9 @@ package org.scaleadvisor.backend.api
 
 import io.swagger.v3.oas.annotations.tags.Tag
 import org.scaleadvisor.backend.api.response.SuccessResponse
+import org.scaleadvisor.backend.project.api.request.UpdateMemberRoleRequest
 import org.scaleadvisor.backend.project.api.response.GetAllProjectMemberResponse
+import org.scaleadvisor.backend.project.api.response.UpdateProjectMemberResponse
 import org.springframework.http.HttpStatus
 import org.springframework.web.bind.annotation.*
 
@@ -16,4 +18,10 @@ interface ProjectMemberAPI {
                 @RequestParam(name = "offset", defaultValue = "0") offset: Int,
                 @RequestParam(name = "limit", defaultValue = "20") limit: Int
     ): SuccessResponse<GetAllProjectMemberResponse>
+
+    @PatchMapping("/{projectId}/users/role")
+    @ResponseStatus(HttpStatus.OK)
+    fun updateMemberRole(@PathVariable projectId: Long,
+                         @RequestBody request: UpdateMemberRoleRequest
+    ): SuccessResponse<UpdateProjectMemberResponse>
 }
