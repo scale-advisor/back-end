a051deeb54c0c17b8977cec7ac530d33b5cc5724
wambatcode <feyfey927@gmail.com>
2025-05-24 19:30:54 +0900

    fix: UpdateMemberStatePort 반환값 변경

--- src/main/kotlin/org/scaleadvisor/backend/project/application/port/repository/member/UpdateMemberStatePort.kt
+++ src/main/kotlin/org/scaleadvisor/backend/project/application/port/repository/member/UpdateMemberStatePort.kt
@@ -1,8 +1,9 @@
 package org.scaleadvisor.backend.project.application.port.repository.member
 
+import org.scaleadvisor.backend.project.domain.ProjectMember
 import org.scaleadvisor.backend.project.domain.enum.MemberState
 import org.scaleadvisor.backend.project.domain.id.ProjectId
 
 fun interface UpdateMemberStatePort {
-    fun updateState(userId: Long, projectId: ProjectId, newState: MemberState): MemberState?
+    fun updateState(userId: Long, projectId: ProjectId, newState: MemberState): ProjectMember?
 }
