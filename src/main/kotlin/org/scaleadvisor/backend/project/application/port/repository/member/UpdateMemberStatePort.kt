e4b2ca1440b1e48c71a32ceaa749b5cd15f21ce4
wambatcode <feyfey927@gmail.com>
2025-05-24 19:12:51 +0900

    rename: UpdateProjectMemberStatePort 파일명 변경

--- src/main/kotlin/org/scaleadvisor/backend/project/application/port/repository/member/UpdateMemberStatePort.kt
+++ src/main/kotlin/org/scaleadvisor/backend/project/application/port/repository/member/UpdateMemberStatePort.kt
@@ -3,6 +3,6 @@ package org.scaleadvisor.backend.project.application.port.repository.member
 import org.scaleadvisor.backend.project.domain.enum.MemberState
 import org.scaleadvisor.backend.project.domain.id.ProjectId
 
-fun interface UpdateProjectMemberStatePort {
+fun interface UpdateMemberStatePort {
     fun updateState(userId: Long, projectId: ProjectId, newState: MemberState): MemberState?
 }
