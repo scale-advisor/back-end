237ab888032cd888f7eca47c70b25e3ef00c36bb
wambatcode <feyfey927@gmail.com>
2025-05-26 01:01:34 +0900

    feat: ProjectMember 관리 권한(Owner) 검증 적용

--- src/main/kotlin/org/scaleadvisor/backend/project/application/service/member/DeleteMemberService.kt
+++ src/main/kotlin/org/scaleadvisor/backend/project/application/service/member/DeleteMemberService.kt
@@ -1,7 +1,9 @@
 package org.scaleadvisor.backend.project.application.service.member
 
+import org.scaleadvisor.backend.global.exception.model.ForbiddenException
 import org.scaleadvisor.backend.global.exception.model.NotFoundException
 import org.scaleadvisor.backend.project.application.port.repository.member.DeleteProjectMemberPort
+import org.scaleadvisor.backend.project.application.port.usecase.member.CheckIsOwnerUseCase
 import org.scaleadvisor.backend.project.application.port.usecase.member.DeleteMemberUseCase
 import org.scaleadvisor.backend.project.application.port.usecase.project.GetProjectUseCase
 import org.scaleadvisor.backend.project.domain.id.ProjectId
@@ -10,9 +12,14 @@ import org.springframework.stereotype.Service
 @Service
 private class DeleteMemberService(
     private val deleteProjectMemberPort: DeleteProjectMemberPort,
-    private val getProjectUseCase: GetProjectUseCase
+    private val getProjectUseCase: GetProjectUseCase,
+    private val checkIsOwnerUseCase: CheckIsOwnerUseCase
 ): DeleteMemberUseCase {
     override fun delete(email: String, projectId: Long) {
+        if (!checkIsOwnerUseCase.checkIsOwner(projectId)) {
+            throw ForbiddenException("프로젝트 요소를 수정할 권한이 없습니다.")
+        }
+
         getProjectUseCase.find(ProjectId.of(projectId))
             ?: throw NotFoundException("프로젝트 $projectId 가 존재하지 않습니다.")
 
--- src/main/kotlin/org/scaleadvisor/backend/project/application/service/member/UpdateMemberRoleService.kt
+++ src/main/kotlin/org/scaleadvisor/backend/project/application/service/member/UpdateMemberRoleService.kt
@@ -1,6 +1,8 @@
 package org.scaleadvisor.backend.project.application.service.member
 
+import org.scaleadvisor.backend.global.exception.model.ForbiddenException
 import org.scaleadvisor.backend.project.application.port.repository.member.UpdateMemberRolePort
+import org.scaleadvisor.backend.project.application.port.usecase.member.CheckIsOwnerUseCase
 import org.scaleadvisor.backend.project.application.port.usecase.member.UpdateMemberRoleUseCase
 import org.scaleadvisor.backend.project.domain.ProjectMember
 import org.springframework.stereotype.Service
@@ -9,15 +11,23 @@ import org.springframework.transaction.annotation.Transactional
 @Service
 @Transactional
 private class UpdateMemberRoleService(
+    private val checkIsOwnerUseCase: CheckIsOwnerUseCase,
     private val updateMemberRolePort: UpdateMemberRolePort
 ): UpdateMemberRoleUseCase {
 
     override fun update(command: UpdateMemberRoleUseCase.UpdateMemberRoleCommand
-    ): ProjectMember? =
-        updateMemberRolePort.updateRole(
+    ): ProjectMember? {
+        if (!checkIsOwnerUseCase.checkIsOwner(command.projectId.toLong())) {
+            throw ForbiddenException("멤버 역할을 수정할 권한이 없습니다.")
+        }
+
+        return updateMemberRolePort.updateRole(
             command.email,
             command.projectId,
             command.newRole
         )
+    }
+
+
 
 }
