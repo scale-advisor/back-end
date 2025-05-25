061fc0a59101c89b7d31b8d389f485552214deca
wambatcode <feyfey927@gmail.com>
2025-05-26 00:23:45 +0900

    refactor: CheckIsEditor UseCase 및 Service 파라미터 수정

--- src/main/kotlin/org/scaleadvisor/backend/project/application/service/member/CheckIsEditorService.kt
+++ src/main/kotlin/org/scaleadvisor/backend/project/application/service/member/CheckIsEditorService.kt
@@ -1,5 +1,7 @@
 package org.scaleadvisor.backend.project.application.service.member
 
+import org.scaleadvisor.backend.global.exception.model.UnauthorizedException
+import org.scaleadvisor.backend.global.security.CurrentUserIdExtractor
 import org.scaleadvisor.backend.project.application.port.repository.member.CheckIsEditorPort
 import org.scaleadvisor.backend.project.application.port.usecase.member.CheckIsEditorUseCase
 import org.springframework.stereotype.Service
@@ -11,7 +13,10 @@ private class CheckIsEditorService(
     private val checkIsEditorPort: CheckIsEditorPort
 ): CheckIsEditorUseCase {
 
-    override fun checkIsEditor(projectId: Long, userId: Long): Boolean {
-        return checkIsEditorPort.checkIsEditor(projectId, userId)
+    override fun checkIsEditor(projectId: Long): Boolean {
+        val currentUserId = CurrentUserIdExtractor.getCurrentUserIdFromSecurity()
+            ?: throw UnauthorizedException("만료 되거나 잘못된 인증 입니다.")
+
+        return checkIsEditorPort.checkIsEditor(projectId, currentUserId)
     }
 }
