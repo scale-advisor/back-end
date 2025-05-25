4e6611b3c3ea4af7604d5d088cea542b184c4c56
wambatcode <feyfey927@gmail.com>
2025-05-25 22:48:21 +0900

    fix: 프론트 테스트를 위한 User Soft -> Hard Delete 수정

--- src/main/kotlin/org/scaleadvisor/backend/user/service/UserService.kt
+++ src/main/kotlin/org/scaleadvisor/backend/user/service/UserService.kt
@@ -1,5 +1,6 @@
 package org.scaleadvisor.backend.user.service
 
+import jakarta.servlet.http.HttpServletResponse
 import org.scaleadvisor.backend.global.config.SecurityConfig
 import org.scaleadvisor.backend.global.exception.constant.UserMessageConstant
 import org.scaleadvisor.backend.global.exception.model.NotFoundException
@@ -45,7 +46,7 @@ class UserService(
         )
     }
 
-    fun deleteUser(request: DeleteUserRequest) {
+    fun deleteUser(request: DeleteUserRequest, response: HttpServletResponse) {
         val user = userRepository.findById(currentUserId)
             ?: throw NotFoundException(String.format(UserMessageConstant.NOT_FOUND_USER_ID_MESSAGE, currentUserId))
 
@@ -53,6 +54,10 @@ class UserService(
             throw ValidationException(UserMessageConstant.INVALID_CREDENTIALS_MESSAGE)
         }
 
-        userRepository.deleteUser(currentUserId)
+        userRepository.delete(currentUserId)
+        response.setHeader(
+            "Set-Cookie",
+            "refreshToken=; HttpOnly; Secure; SameSite=None; Domain=; Path=/; Max-Age=0"
+        )
     }
 }
