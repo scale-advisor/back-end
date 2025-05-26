0521a1179bf16ece6f24f4fc6c93a86dcb6bc3a0
dj.jeong <sam1087@mju.ac.kr>
2025-05-27 00:46:40 +0900

    feat: 프로젝트 설정 기능 유형 가중치가 없을때, 정부 표준값 사용하도록 구성

--- src/main/kotlin/org/scaleadvisor/backend/project/application/port/usecase/fpweights/GetFpWeightsUseCase.kt
+++ src/main/kotlin/org/scaleadvisor/backend/project/application/port/usecase/fpweights/GetFpWeightsUseCase.kt
@@ -3,6 +3,6 @@ package org.scaleadvisor.backend.project.application.port.usecase.fpweights
 import org.scaleadvisor.backend.project.domain.FpWeights
 import org.scaleadvisor.backend.project.domain.id.ProjectId
 
-fun interface FindFpWeightsUseCase {
+fun interface GetFpWeightsUseCase {
     fun find(projectId: ProjectId): FpWeights?
 }
