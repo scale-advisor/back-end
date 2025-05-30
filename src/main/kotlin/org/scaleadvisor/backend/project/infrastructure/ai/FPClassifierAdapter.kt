f3148464f2de09b28a3accebc5f1272344f51aa3
dj.jeong <sam1087@mju.ac.kr>
2025-05-31 00:48:42 +0900

    refactor: sonar cloud 이슈 사항 개선

--- src/main/kotlin/org/scaleadvisor/backend/project/infrastructure/ai/FPClassifierAdapter.kt
+++ src/main/kotlin/org/scaleadvisor/backend/project/infrastructure/ai/FPClassifierAdapter.kt
@@ -2,8 +2,6 @@ package org.scaleadvisor.backend.project.infrastructure.ai
 
 import org.scaleadvisor.backend.project.application.port.repository.unitprocess.ClassifyUnitProcessPort
 import org.scaleadvisor.backend.project.domain.UnitProcess
-import org.scaleadvisor.backend.project.domain.enum.FunctionType
-import org.scaleadvisor.backend.project.domain.id.UnitProcessId
 import org.scaleadvisor.backend.project.infrastructure.ai.client.FPClassifierClient
 import org.scaleadvisor.backend.project.infrastructure.ai.client.FPClassifierClientRequest
 import org.springframework.stereotype.Component
@@ -14,7 +12,6 @@ private class FPClassifierAdapter(
 ) : ClassifyUnitProcessPort {
 
     fun classify(unitProcessList: List<UnitProcess>): List<UnitProcess> {
-        val results: List<UnitProcess>
         val requests: List<FPClassifierClientRequest>  = unitProcessList.map {
             FPClassifierClientRequest(
                 it.name,
@@ -23,13 +20,17 @@ private class FPClassifierAdapter(
         }
         val unitProcessIdMap = unitProcessList.associateBy { it.id.toString() }
         try {
-            return fpClassifierClient.classify(requests).map {
+            return fpClassifierClient.classify(requests).map { response ->
+                val originalUnitProcess = unitProcessIdMap[response.unitProcessId]
+                if (originalUnitProcess != null) {
                 UnitProcess(
-                    id = UnitProcessId.from(it.unitProcessId),
-                    name = it.unitProcess,
-                    functionType = FunctionType.valueOf(it.functionType),
-                    isAmbiguous = unitProcessIdMap[it.unitProcessId]!!.isAmbiguous,
-                )
+                    id = originalUnitProcess.id,
+                    name = originalUnitProcess.name,
+                    functionType = originalUnitProcess.functionType,
+                    isAmbiguous = originalUnitProcess.isAmbiguous,
+                )} else {
+                    throw RuntimeException("응답에서 알 수 없는 unitProcessId를 받았습니다: ${response.unitProcessId}")
+                }
             }
         } catch (ex: Exception) {
             // 로깅 또는 예외 처리
